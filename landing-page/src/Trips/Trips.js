c7d81eeed25167bf4cec0bb3309670e96730686a
Csilla <posa.csilla@gmail.com>
2020-07-07 17:44:29 +0200

    Style changes
    Added a new background, created a paddle shaped button which changes color when we hover over it, added changes to the trip offers.

--- landing-page/src/Trips/Trips.js
+++ landing-page/src/Trips/Trips.js
@@ -3,9 +3,9 @@ import React from 'react';
 const trips = (props) => {
     return(
         <div className="trips">
-            <h2>Trip name: {props.name}</h2>
-            <p>Trip length: {props.length}</p>
-            <p>Trip price: {props.price}</p>
+            <h2 className="trip-name">Trip name: {props.name}</h2>
+            <p className="trip-length">Trip length: {props.length}</p>
+            <p className="trip-price">Trip price: {props.price}</p>
             <button>Buy now</button>
         </div>
     )
